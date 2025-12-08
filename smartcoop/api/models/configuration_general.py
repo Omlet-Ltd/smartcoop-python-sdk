f2df4687a923001ba6dc94a56700df66b72cdf3a
Dave Legg <dave@omlet.co.uk>
2025-12-08 07:49:22 +0000

    Consistent ordering of parameters, version bump

--- smartcoop/api/models/configuration_general.py
+++ smartcoop/api/models/configuration_general.py
@@ -21,13 +21,13 @@ class ConfigurationGeneral:
             datetime=json_data['datetime'],
             timezone=json_data['timezone'],
             updateFrequency=json_data['updateFrequency'],
+            statusUpdatePeriod=json_data['statusUpdatePeriod'],
             language=json_data['language'] if json_data.get('language') else None,
             overnightSleepEnable=json_data['overnightSleepEnable'] if json_data.get('overnightSleepEnable') else None,
             overnightSleepStart=json_data['overnightSleepStart'] if json_data.get('overnightSleepStart') else None,
             overnightSleepEnd=json_data['overnightSleepEnd'] if json_data.get('overnightSleepEnd') else None,
             pollFreq=json_data['pollFreq'] if json_data.get('pollFreq') else None,
             stayAliveTime=json_data['stayAliveTime'] if json_data.get('stayAliveTime') else None,
-            statusUpdatePeriod=json_data['statusUpdatePeriod'],
             useDst=json_data['useDst'] if json_data.get('useDst') else None
         )
 
@@ -36,12 +36,12 @@ class ConfigurationGeneral:
             "datetime": self.datetime,
             "timezone": self.timezone,
             "updateFrequency": self.updateFrequency,
+            "stayAliveTime": self.stayAliveTime,
             "language": self.language,
             "overnightSleepEnable": self.overnightSleepEnable,
             "overnightSleepStart": self.overnightSleepStart,
             "overnightSleepEnd": self.overnightSleepEnd,
             "pollFreq": self.pollFreq,
-            "stayAliveTime": self.stayAliveTime,
             "statusUpdatePeriod": self.statusUpdatePeriod,
             "useDst": self.useDst
         }
--- smartcoop/api/models/state_general.py
+++ smartcoop/api/models/state_general.py
@@ -18,9 +18,9 @@ class StateGeneral:
             firmwareVersionCurrent=json_data['firmwareVersionCurrent'],
             firmwareVersionPrevious=json_data['firmwareVersionPrevious'],
             firmwareLastCheck=json_data['firmwareLastCheck'],
-            batteryLevel=json_data['batteryLevel'] if json_data.get('batteryLevel') else None,
             powerSource=json_data['powerSource'],
             uptime=json_data['uptime'],
+            batteryLevel=json_data['batteryLevel'] if json_data.get('batteryLevel') else None,
             displayLine1=json_data['displayLine1'] if json_data.get('displayLine1') else None,
             displayLine2=json_data['displayLine2'] if json_data.get('displayLine2') else None
         )
