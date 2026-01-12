5419523fa7c2387729734af07c2ff0e3b983f4d0
Dave Legg <dave@omlet.co.uk>
2026-01-12 09:01:19 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smartcoop_python_sdk',
-    version='1.1.6',
+    version='1.1.7',
     packages=find_packages(),
     install_requires=[
         'requests',
